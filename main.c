05837d197501f6ca65443105f9c5a311db96dcd3
Abdulkarim Saddik <abdulkarim.saddik@gmail.com>
2021-01-06 01:00:47 +0100

    make the coder simpler to read in main
    wrap code from main.c in quit_program() function

--- main.c
+++ main.c
@@ -5,13 +5,16 @@ The user adds an arithmetic expression which is calculated by the program
 *******************************************************************************/
 
 #include "calculator.h"
+
 #include <stdio.h>
+
 #include <stdbool.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
 int main() {
-    int count;
     double result;
     bool close_program = false;
     char ** res_str = (char ** ) malloc(sizeof(char * ));
@@ -24,44 +27,23 @@ int main() {
         return memory_allocation_failed;
     }
 
-    /*allocate memory for several substrings 'Num_Strings'
-     * currently there are only one substring */
-    for (count = 0; count < Num_Strings; count++) {
-        *(my_input + count) = (char * ) malloc(sizeof(char) * STRING_SIZE);
-        if (! * (my_input + count)) {
-            printf("memory allocation failed\n"
-                "exit the program\n");
-            return memory_allocation_failed;
-        }
+    * my_input = (char * ) malloc(sizeof(char) * STRING_SIZE);
+    if (! * my_input) {
+        printf("memory allocation failed\n"
+            "exit the program\n");
+        return memory_allocation_failed;
     }
 
     while (1) {
-        /* the purpose of using this for loop is for future development. The goal is to save several inputs from the user
-	  and enable the user from calling his input history*/
-        for (count = 0; count < Num_Strings; count++) {
-            /*communicate with the user and read the input from the terminal*/
-            print_func(my_input);
-
-            /*if the user inserted 'q' or 'quit', the program should close*/
-            if ((!strncmp( * (my_input + count), "q", 1) && strlen(my_input[0])==2) ||
-                (!strncmp( * (my_input + count), "quit", 4)  && strlen(my_input[0])==5)) {
-                printf("quit the program\n");
-                close_program = true;
-                break;
-            }
-            /*parse the input and output the result*/
-            string_parse( * (my_input + count), res_str[0], & result);
-        }
-        /*check this flag to close the program in a good manner*/
+        print_func(my_input);
+        close_program = quit_program(my_input[0]);
         if (close_program) {
             break;
         }
+        /*parse the input and output the result*/
+        string_parse(my_input[0], res_str[0], & result);
     }
     free(res_str);
     free(my_input);
-    /*free heap memory for all substrings*/
-    for (count = 0; count < Num_Strings; count++) {
-        free( * (my_input + count));
-    }
     return EXIT_SUCCESS;
 }
